5f016551eae91fe610287076fddf1ecb930825e1
Selma <amcassouselma@gmail.com>
2021-05-19 23:03:08 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {CatalogueService} from './catalogue.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  public categories;
+  constructor(private catalogueService : CatalogueService){
+
+  }
+  ngOnInit(): void {
+    this.getCategories();
+  }
+  getCategories() {
+    this.catalogueService.getResource("/categories").subscribe(data=>{
+      this.categories=data;
+    },error=>{
+      console.log(error);
+    })
+  }
   title = 'angularfrontend';
 }
